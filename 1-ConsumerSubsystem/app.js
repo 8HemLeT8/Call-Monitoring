31773fa12ca96120d2e607b46f7691dc865aaa2d
שניר יפת <snirycr7@gmail.com>
2020-08-12 16:48:59 +0300

    set infrastructure for two new kafka consumers subsystems

---
+++ 1-ConsumerSubsystem/app.js
@@ -0,0 +1,33 @@
+// INCLUDE
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// MACRO
+const PORT = 4000;
+
+// GLOBAL
+
+// set rendering to be with ejs
+app.set("view engine", "ejs");
+
+// to enablue use of static files like css files
+app.use(express.static("public"));
+
+// ROUTING
+
+app.get("/",function (req,res) {
+    res.send("Consumer 1 Home Page");
+  });
+
+
+// server is listening on port 3000
+server.listen(PORT || process.env.PORT, function () {
+    console.log("Server is running on port: " + PORT);
+  });
